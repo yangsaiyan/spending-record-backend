31d66e08480e6249987da7c4b486e42c098fe3ba
unknown <yangz030715@gmail.com>
2025-07-18 13:54:04 +0800

    feat: cors allow all origin

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors({
+    origin: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
