cbc1f671fd345c7176e390f55fc0ba655523fe5d
Lim Zi Yang <yangz030715@gmail.com>
2025-07-19 22:17:10 +0800

    fix: record module import

--- src/record/record.module.ts
+++ src/record/record.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RecordService } from './record.service';
 import { RecordController } from './record.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Record } from './record.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Record])],
   controllers: [RecordController],
   providers: [RecordService],
 })
