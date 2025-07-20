fd224c64cd409d669e95fed1a218d06874959fa8
Lim Zi Yang <yangz030715@gmail.com>
2025-07-20 14:42:20 +0800

    fix: export OtpService

--- src/otp/otp.module.ts
+++ src/otp/otp.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OtpService } from './otp.service';
 import { OtpController } from './otp.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Otp } from './otp.entity';
 
 @Module({
   controllers: [OtpController],
   providers: [OtpService],
+  imports: [TypeOrmModule.forFeature([Otp])],
+  exports: [OtpService],
 })
 export class OtpModule {}
